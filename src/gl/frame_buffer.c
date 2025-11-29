2fa0c10bb67936524da62f59b9a7f8ac3ef1ec4b
Christian377 <christianelieabboud@gmail.com>
2025-11-29 14:22:58 +0100

    Added multisampling support for frame buffers and textures.

--- src/gl/frame_buffer.c
+++ src/gl/frame_buffer.c
@@ -2,16 +2,35 @@
 
 void aa_texture_create(aa_texture* out, size_t width, size_t height)
 {
+  out->target = GL_TEXTURE_2D;
   glCall(glGenTextures(1, &out->id));
+  glCall(glBindTexture(out->target, out->id));
   glCall(glTexImage2D(
       GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
 
+void aa_texture_msaa_create(
+    aa_texture* out)
+{
+  out->target = GL_TEXTURE_2D_MULTISAMPLE;
+  glCall(glGenTextures(1, &out->id));
+}
+//void aa_texture_msaa_create(
+//    aa_texture* out, size_t width, size_t height, uint8_t samples)
+//{
+//  out->target = GL_TEXTURE_2D_MULTISAMPLE;
+//  glCall(glGenTextures(1, &out->id));
+//  glCall(glBindTexture(out->target, out->id));
+//
+//  glCall(glTexImage2DMultisample(
+//      out->target, samples, GL_RGBA8, width, height, GL_TRUE));
+//}
+
 void aa_texture_bind(aa_texture* out)
 {
-  glCall(glBindTexture(GL_TEXTURE_2D, out->id));
+  glCall(glBindTexture(out->target, out->id));
 }
 
 void aa_texture_delete(aa_texture* out)
@@ -19,8 +38,17 @@ void aa_texture_delete(aa_texture* out)
   glCall(glDeleteTextures(1, &out->id));
 }
 
-void aa_texture_dimensions(aa_texture* out, size_t width, size_t height)
+void aa_texture_msaa_dimensions(
+    aa_texture* out, size_t width, size_t height, uint8_t samples)
 {
+  glCall(glBindTexture(out->target, out->id));
+  glCall(glTexImage2DMultisample(
+      out->target, samples, GL_RGBA8, width, height, GL_TRUE));
+}
+
+void aa_texture_2D_dimensions(aa_texture* out, size_t width, size_t height)
+{
+  glCall(glBindTexture(out->target, out->id));
   glCall(glTexImage2D(
       GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
 }
@@ -40,8 +68,26 @@ void aa_frame_buffer_delete(aa_frame_buffer* out)
   glCall(glDeleteFramebuffers(1, &out->id));
 }
 
-void aa_bind_color_texture(aa_frame_buffer* out, aa_texture* texture)
+void aa_frame_buffer_blit(
+    aa_frame_buffer* out, aa_frame_buffer* multisample_fbo, size_t width,
+    size_t height)
 {
-  glCall(glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture->id, 0));
+  glBindFramebuffer(GL_READ_FRAMEBUFFER, multisample_fbo->id);
+  glBindFramebuffer(GL_DRAW_FRAMEBUFFER, out->id);
+
+  glCall(glBlitFramebuffer(
+      0, 0, width, height, 0, 0, width, height,
+      GL_COLOR_BUFFER_BIT, GL_NEAREST));
 }
 
+void aa_frame_buffer_color_texture(aa_frame_buffer* out, aa_texture* texture)
+{
+  glCall(glBindFramebuffer(GL_FRAMEBUFFER, out->id));
+  glCall(glFramebufferTexture2D(
+      GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, texture->target, texture->id, 0));
+  GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+  if (status != GL_FRAMEBUFFER_COMPLETE)
+  {
+    printf("FBO error: 0x%x\n", status);
+  }
+}
--- src/gl/frame_buffer.h
+++ src/gl/frame_buffer.h
@@ -2,28 +2,35 @@
 #define __HG_AA_GL_FRAME_BUFFER
 
 #include <glad/glad.h>
+#include <stdint.h>
 #include "error.h"
 
 typedef struct
 {
   unsigned int id;
-}aa_texture;
+  GLenum target;
+} aa_texture;
 
 void aa_texture_create(aa_texture* out, size_t width, size_t height);
+void aa_texture_msaa_create(
+    aa_texture* out);
 void aa_texture_bind(aa_texture* out);
 void aa_texture_delete(aa_texture* out);
 void aa_texture_dimensions(aa_texture* out, size_t width, size_t height);
+void aa_texture_msaa_dimensions(
+    aa_texture* out, size_t width, size_t height, uint8_t samples);
 
 typedef struct
 {
   unsigned int id;
-}aa_frame_buffer;
+} aa_frame_buffer;
 
 void aa_frame_buffer_create(aa_frame_buffer* out);
 void aa_frame_buffer_bind(aa_frame_buffer* out);
 void aa_frame_buffer_delete(aa_frame_buffer* out);
-void aa_bind_color_texture(aa_frame_buffer* out, aa_texture texture);
-
-
+void aa_frame_buffer_color_texture(aa_frame_buffer* out, aa_texture* texture);
+void aa_frame_buffer_blit(
+    aa_frame_buffer* out, aa_frame_buffer* multisample_fbo, size_t width,
+    size_t height);
 
 #endif // !__HG_AA_GL_FRAME_BUFFER
