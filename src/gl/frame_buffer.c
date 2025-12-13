ee4306a6c12e80b84b0ef4624d8904c78d4db022
Christian377 <christianelieabboud@gmail.com>
2025-12-14 00:31:34 +0100

    Modified texture clamping parameters, added specific function for smaa search and area textures.

--- src/gl/frame_buffer.c
+++ src/gl/frame_buffer.c
@@ -6,13 +6,15 @@ void aa_texture_create(aa_texture* out, size_t width, size_t height)
   glCall(glGenTextures(1, &out->id));
   glCall(glBindTexture(out->target, out->id));
   glCall(glTexImage2D(
-      GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
+      GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE,
+      NULL));
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 }
 
-void aa_texture_msaa_create(
-    aa_texture* out)
+void aa_texture_msaa_create(aa_texture* out)
 {
   out->target = GL_TEXTURE_2D_MULTISAMPLE;
   glCall(glGenTextures(1, &out->id));
@@ -33,16 +35,44 @@ void aa_texture_from_data(
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 }
-//void aa_texture_msaa_create(
-//    aa_texture* out, size_t width, size_t height, uint8_t samples)
-//{
-//  out->target = GL_TEXTURE_2D_MULTISAMPLE;
-//  glCall(glGenTextures(1, &out->id));
-//  glCall(glBindTexture(out->target, out->id));
-//
-//  glCall(glTexImage2DMultisample(
-//      out->target, samples, GL_RGBA8, width, height, GL_TRUE));
-//}
+
+void aa_smaa_search_texture(
+    aa_texture* out, const unsigned char* data, int width, int height)
+{
+  out->target = GL_TEXTURE_2D;
+  glGenTextures(1, &out->id);
+  glBindTexture(out->target, out->id);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  // Standard SearchTex is single channel (Grayscale)
+  glTexImage2D(
+      GL_TEXTURE_2D, 0, GL_R8, (GLsizei)width, (GLsizei)height, 0, GL_RED,
+      GL_UNSIGNED_BYTE, data);
+}
+
+void aa_smaa_area_texture(
+    aa_texture* out, const unsigned char* data, int width, int height)
+{
+  out->target = GL_TEXTURE_2D;
+  glGenTextures(1, &out->id);
+  glBindTexture(out->target, out->id);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+  // AreaTex is two channels
+  glTexImage2D(
+      GL_TEXTURE_2D, 0, GL_RG8, (GLsizei)width, (GLsizei)height, 0, GL_RG,
+      GL_UNSIGNED_BYTE, data);
+}
 
 void aa_texture_bind(aa_texture* out)
 {
@@ -66,7 +96,7 @@ void aa_texture_dimensions(aa_texture* out, size_t width, size_t height)
 {
   glCall(glBindTexture(out->target, out->id));
   glCall(glTexImage2D(
-      GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
+      GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL));
 }
 
 void aa_frame_buffer_create(aa_frame_buffer* out)
@@ -92,8 +122,7 @@ void aa_frame_buffer_blit(
   glBindFramebuffer(GL_DRAW_FRAMEBUFFER, out->id);
 
   glCall(glBlitFramebuffer(
-      0, 0, width, height, 0, 0, width, height,
-      GL_COLOR_BUFFER_BIT, GL_NEAREST));
+      0, 0, width, height, 0, 0, width, height, GL_COLOR_BUFFER_BIT, GL_NEAREST));
 }
 
 void aa_frame_buffer_color_texture(aa_frame_buffer* out, aa_texture* texture)
